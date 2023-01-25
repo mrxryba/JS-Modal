adbd311a59784d06bf7e273e8c07a77e1202b2af
mrxryba <marcinrybakx@gmail.com>
2023-01-25 14:36:07 +0100

    Created JS Modal

---
+++ inc/modal.js
@@ -0,0 +1,22 @@
+
+
+const openBtn = document.getElementById('open-btn');
+
+const  closeBtn = document.getElementById('close-btn');
+
+const modalContainer = document.getElementById('modal-container');
+
+
+openBtn.addEventListener('click',function () {
+    modalContainer.style.display = "block";
+});
+
+closeBtn.addEventListener('click',function () {
+    modalContainer.style.display = "none";
+})
+
+window.addEventListener('click',function (e) {
+    if (e.target === modalContainer) {
+        modalContainer.style.display = "none";
+    }
+})
